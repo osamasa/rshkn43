2637b51e80094b5e43508e3cd2a8987cfc51e2b8
osada <osamasa@gmail.com>
2023-09-16 19:24:31 +0900

    b

--- pages/setting.vue
+++ pages/setting.vue
@@ -5,7 +5,7 @@
         <v-btn color="success" block @click="gostart">新規作成</v-btn>
       </v-col>
     </v-row>
-    <v-row>
+    <v-row v-if="hasHistory">
       <v-col>    
         <v-btn color="info" block @click="recover">前回の続き</v-btn>
       </v-col>
@@ -21,6 +21,7 @@ const runtimeConfig = useRuntimeConfig();
 const router = useRouter();
 const islogin = ref(false);
 const isClient = ref(false);
+const hasHistory = ref(true);
 const { userid, updateUserid } = useUserid();
 const { loading, updateLoading} = useLoading();
 const { updateErrorMsg } = useErrorMsg();
@@ -32,12 +33,12 @@ updateLoading(true);
 const myloginCheck = () => {
       // ログインチェック
       if (liff.isLoggedIn()) {
-        isClient.value = liff.isInClient()
-        // プロフィール取得
-        liff.getProfile()
+          isClient.value = liff.isInClient()
+          // プロフィール取得
+          liff.getProfile()
               .then(profile => {
                   updateUserid(profile.userId);
-                  updateLoading(false);   
+                  hasHistoryFnc(profile.userId);
                   snackbartext.value="ログイン完了"
                   snackbarboolean.value=true;
               })
@@ -53,6 +54,7 @@ onMounted(() => {
     liff.init({ liffId: runtimeConfig.public.liffId },
               myloginCheck,
               errorCallback);
+    updateLoading(false);       
 });
 
 
@@ -60,9 +62,26 @@ const errorCallback = (err)=>{
     updateErrorMsg('[setting.vue][ERR103]' + err);
 };    
 
+const hasHistoryFnc = async (_userid) => {
+    const { count,error } = await supabase
+          .from('games_view')
+          .select('*',{ count: 'exact' })
+          .eq('loginid', _userid)
+    if(error) {
+        updateErrorMsg('[setting.vue][ERR108]' + error.code + ' ' + error.message);
+        hasHistory.value = false;
+    } else {
+        if(count > 0) {
+            hasHistory.value = true;
+        } else {
+            hasHistory.value = false;
+        }
+    }
+}
+
 const gostart = async () => {
     updateLoading(true);
-    const { data, error,count } = await supabase
+    const { data, error } = await supabase
           .from('users')
           .select('*', { count: 'exact' })
           .eq('loginid', userid.value)
@@ -91,25 +110,15 @@ const gostart = async () => {
 const recover = async () => {
     updateLoading(true);
     const { data, error } = await supabase
-          .from('users')
-          .select('id')
+          .from('games_view')
+          .select('gameid')
           .eq('loginid',userid.value)
           .single()
     if(error) {
         updateErrorMsg('[setting.vue][ERR105]' + error.code + ' ' + error.message);
         updateLoading(false);        
     } else {
-        const { data, error } = await supabase
-              .from('games')
-              .select('id')
-              .eq('userid',userid.value)
-              .single()        
-        if(error) {
-            updateErrorMsg('[setting.vue][ERR106]' + error.code + ' ' + error.message);
-            updateLoading(false);
-        } else {
-            router.push('/'+ data.id)
-        }
+        router.push('/'+ data.gameid)
     }
 };
 
